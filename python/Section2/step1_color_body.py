4cb4c74bc685f5fd28399a83545d83196805e46e
Cem C. Tutum <cem.celal@gmail.com>
2024-10-06 14:26:04 -0500

    added cleaned journal script for changing the color of a part with a specific name to be automated at the next step

---
+++ python/Section2/step1_color_body.py
@@ -0,0 +1,26 @@
+﻿# intellisense by theScriptingEngineer (www.theScriptingEngineer.com)
+# Created by theScriptingEngineer
+#
+import NXOpen
+def main() : 
+    theSession  = NXOpen.Session.GetSession()
+    workPart = theSession.Parts.Work
+    displayPart = theSession.Parts.Display
+    
+    displayModification1 = theSession.DisplayManager.NewDisplayModification()
+    displayModification1.ApplyToAllFaces = True
+    displayModification1.ApplyToOwningParts = False
+    displayModification1.NewColor = 1
+    
+    objects1 = [NXOpen.DisplayableObject.Null] * 1 
+    body1 = workPart.Bodies.FindObject("EXTRUDE(2)")
+    objects1[0] = body1
+    displayModification1.Apply(objects1)
+    
+    markId4 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "Edit Object Display")
+    nErrs1 = theSession.UpdateManager.DoUpdate(markId4)
+    
+    displayModification1.Dispose()
+    
+if __name__ == '__main__':
+    main()
